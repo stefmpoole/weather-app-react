d11e70b3325dc3b31285f6b4aa242e2657315699
stefmpoole <104706141+stefmpoole@users.noreply.github.com>
2022-08-15 19:50:50 -0400

    switched background image
    switched background image to heading and changed image, updated css, added row and column to app.js, changed color of cel to yellow

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import LocationSearch from "./LocationSearch.js";
 import Footer from "./Footer.js";
 
@@ -22,12 +24,29 @@ function App() {
             padding: 2,
           }}
         >
-          <div className="forecast-heading" style={{ display: "block" }}>
-            <h1>
-              <strong>The Umbrella Forecast</strong>
-            </h1>
-            <h3>A 5-Day Weather Report</h3>
-          </div>
+          <Row>
+            <div className="col-12">
+              <div
+                className="img-fluid rounded"
+                id="img"
+                style={{
+                  backgroundImage:
+                    "url('https://images.pexels.com/photos/1451040/pexels-photo-1451040.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                  height: "20vh",
+                }}
+              />
+            </div>
+          </Row>
+          <Row>
+            <div className="col-12">
+              <h1>
+                <strong>The Umbrella Forecast</strong>
+              </h1>
+              <h3>A 5-Day Weather Report</h3>
+            </div>
+          </Row>
         </Card>
         <LocationSearch />
         <Footer />
--- src/LocationSearch.js
+++ src/LocationSearch.js
@@ -6,32 +6,11 @@ import "./LocationSearch.css";
 
 export default function LocationSearch() {
   return (
-    <div className="location-search" style={{ paddingTop: 30 }}>
-      <div
-        style={{
-          backgroundImage:
-            "url('https://images.pexels.com/photos/10398361/pexels-photo-10398361.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "contain",
-          height: 400,
-        }}
-      >
-        <overlayView
-          className="mask rounded"
-          style={{
-            color: "white",
-            height: 365,
-            width: 552,
-            backgroundColor: "rgba(2, 1, 0, 0.1)",
-            backgroundSize: "contain",
-          }}
-        >
-          <Col className="col-12 pt-4 text-center">
-            <p className="h3" id="h3"></p>
-          </Col>
-          <Weather />
-        </overlayView>
-      </div>
+    <div className="location-search" style={{ paddingTop: 5 }}>
+      <Col className="col-12 text-center">
+        <p className="h3" id="h3"></p>
+      </Col>
+      <Weather />
     </div>
   );
 }
