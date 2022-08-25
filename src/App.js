e0e16401c95c09fe4ee59151752af0668d32b539
stefmpoole <104706141+stefmpoole@users.noreply.github.com>
2022-08-25 13:04:39 -0400

    changed vh
    changed background img and mask vh to 135vh

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           "url('https://images.pexels.com/photos/5855878/pexels-photo-5855878.jpeg?cs=srgb&dl=pexels-roman-odintsov-5855878.jpg&fm=jpg')",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
-        height: "100vh",
+        height: "135vh",
         maxwidth: 600,
       }}
     >
@@ -24,7 +24,7 @@ function App() {
         style={{
           backgroundColor: "rgba(80, 68, 45, .5)",
           padding: 40,
-          height: "100vh",
+          height: "135vh",
           maxwidth: 600,
         }}
       >
